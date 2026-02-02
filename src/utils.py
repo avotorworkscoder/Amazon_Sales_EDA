280388c1dea5723df5cb9484ac11ddfb43c741e0
avotorworkscoder <avotor.works@gmail.com>
2026-02-02 23:26:22 +0530

    added the output folder

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,5 @@
 def save_plot(name):
     import os
     import matplotlib.pyplot as plt
-    os.makedirs("outputs/plots", exist_ok=True)
-    plt.savefig(f"outputs/plots/{name}.png", dpi=300, bbox_inches="tight")
+    os.makedirs("output/plots", exist_ok=True)
+    plt.savefig(f"output/plots/{name}.png", dpi=300, bbox_inches="tight")
